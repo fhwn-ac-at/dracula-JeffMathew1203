8dc7da0eeccbeb4fdadaea720c059551f73a8446
Jeff <210891@fhwn.ac.at>
2025-06-05 14:48:40 +0200

    Updated pfusch.c

--- pfusch.c
+++ pfusch.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+typedef struct {
+    float x;
+    float y;
+} point;
+
+bool is_point_in_circle(point p)
+{
+    float dx = p.x;
+    float dy = p.y;
+    return (dx * dx + dy * dy) <= 1;
+}
+
+point random_point()
+{
+
+    return (point) {
+        .x = 1 - 2*(float)rand() / (float)RAND_MAX,
+        .y = 1 - 2*(float)rand() / (float)RAND_MAX,
+    };
+}
+
+point random_point_in_circle()
+{
+    point p;
+    do {
+        p = random_point();
+    } while (!is_point_in_circle(p));
+    return p;
+}
+
 
 int main(void) {
-    printf("Hello, World!\n");
+
+    srand(time(NULL));
+
+    point p = random_point();
+
+    if (is_point_in_circle(p)) {
+        printf("The point (%.2f, %.2f) is inside the unit circle.\n", p.x, p.y);
+    } else {
+        printf("The point (%.2f, %.2f) is NOT inside the unit circle.\n", p.x, p.y);
+    }
 
     return 0;
 }
